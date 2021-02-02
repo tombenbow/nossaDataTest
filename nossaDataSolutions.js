5376b4c8b637ee7f8c1341a649f1101fa92b4b93
tombenbow <tombenbow@gmail.com>
2021-02-02 21:04:45 +0000

    second commit

--- nossaDataSolutions.js
+++ nossaDataSolutions.js
@@ -1,16 +1,32 @@
 // Find the Duplicate 
 // I assumed here there always would be a duplicate
 
-const findTheDuplicate = () => {
-    return 0
+const findTheDuplicate = (arr) => {
+    const obj = {}
+    for (i = 0; i < arr.length; i++) {
+        if (obj.hasOwnProperty(arr[i])) {
+            return arr[i]
+        }
+        else {
+            obj[arr[i]] = arr[i]
+        }
+    }
 }
 
 //Longest Consecutive 1s
-// I assumed here the input would always be a positive integer
+// I assumed here the input would always be an integer
 
-const longestConsecutive1s = (x) => {
-    return parseInt(x.toString(2)) //in JS, this converts a number to it's binary representation
+const longestConsecutive1s = (num) => {
+    let numInBinary = num.toString(2) //converts num to its binary represenation
+    for (let i = numInBinary.length; i > 0; i--) {
+        let regex = new RegExp(`1{${i}}`)
+         if (regex.test(numInBinary)) {
+            return i;
+        }
+    }
 }
+
+longestConsecutive1s(0)
 
 module.exports = {
     findTheDuplicate,
